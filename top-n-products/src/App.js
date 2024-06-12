0f9a584a2b9c86714a316d5c26408eb24f8a849a
Praveen <napraveen03@gmail.com>
2024-06-12 15:48:55 +0800

    changed

--- top-n-products/src/App.js
+++ top-n-products/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'http://20.244.56.144/test/companies/AMZ/categories/Laptop/products?top=10&minPrice=1&maxPrice=1000',
+        {
+          method: 'GET',
+          headers: {
+            Authorization:
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE4MTc4NTEwLCJpYXQiOjE3MTgxNzgyMTAsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImQ2ZjlmNGQwLWZhNTAtNGY2Yi1hOGU1LTY4ZWM4NTBkNmQ0MSIsInN1YiI6Im5hcHJhdmVlbjAzQGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6IkFmZm9yZG1lZCIsImNsaWVudElEIjoiZDZmOWY0ZDAtZmE1MC00ZjZiLWE4ZTUtNjhlYzg1MGQ2ZDQxIiwiY2xpZW50U2VjcmV0IjoiQmRjZ1BWSnN5V21wRUh2byIsIm93bmVyTmFtZSI6IlByYXZlZW4gTiIsIm93bmVyRW1haWwiOiJuYXByYXZlZW4wM0BnbWFpbC5jb20iLCJyb2xsTm8iOiIzMTIzMjExMDYxMjkifQ.kzL-FqMJ244HAx37ceQGhnHuPG9a9MMODo7NcGqRmrs',
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      const result = await response.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Datas:</h1>
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
 }
